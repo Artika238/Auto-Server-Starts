824801bb198987193005305f820ca7032a51e524
Artika <artemch238@gmail.com>
2023-08-02 21:25:48 +0300

    Add Logs
    Add base logic for start server LOGs

--- Program.cs
+++ Program.cs
@@ -17,14 +17,15 @@ namespace AutoServers
         {
             while (true)
             {
+                Int64 x;
                 bool isRunning = Process.GetProcessesByName("Torch.Server").FirstOrDefault(p => p.MainModule.FileName.StartsWith(@"C:\SERVER9-HARD")) != default(Process);
                 bool isRunning2 = Process.GetProcessesByName("Torch.Server").FirstOrDefault(t => t.MainModule.FileName.StartsWith(@"C:\SERVER11-GALAXY2")) != default(Process);
                 bool isRunning3 = Process.GetProcessesByName("Torch.Server").FirstOrDefault(t => t.MainModule.FileName.StartsWith(@"C:\TORCH-Server_14")) != default(Process);
                 string exePath1 = @"C:\SERVER9-HARD\Torch.Server.exe";
                 string exePath2 = @"C:\SERVER11-GALAXY2\Torch.Server.exe";
                 string exePath3 = @"C:\TORCH-Server_14\Torch.Server.exe";
-                string filePath = @"G:\Logs Server AutoStart";
-                StringBuilder lg = new StringBuilder();
+                string filePath = @"G:\Logs Server AutoStart\";
+                StreamWriter lg = new StreamWriter(filePath + "logServerAutostarts.log", true, Encoding.ASCII);
                 if (isRunning == true)
                 {
                     Console.WriteLine("Quantum I Ready");
@@ -32,10 +33,15 @@ namespace AutoServers
                 else
                 {
                     Process.Start(exePath1);
+                    lg.WriteLine("\n" + DateTime.Now + "\nQuantum I Started");
+                    for (x = 0; x < 10; x++)
+                    {
+                        lg.Write(x);
+                    }
+
+                    //close the file
+                    lg.Close();
                     Thread.Sleep(10000);
-                    
-                    TimeSpan ts = TimeSpan.Zero;
-                    lg.AppendJoin("Quantum I Started {0}h, {1}m, {2}s.", ts.Hours, ts.Minutes, ts.Seconds);
                 }
                 if (isRunning2 == true)
                 {
@@ -44,9 +50,15 @@ namespace AutoServers
                 else
                 {
                     Process.Start(exePath2);
+                    lg.WriteLine("\n" + DateTime.Now + "\nQuantum II Started");
+                    for (x = 0; x < 10; x++)
+                    {
+                        lg.Write(x);
+                    }
+
+                    //close the file
+                    lg.Close();
                     Thread.Sleep(10000);
-                    TimeSpan ts2 = TimeSpan.Zero;
-                    lg.AppendJoin("Quantum II Started {0}h, {1}m, {2}s.", ts2.Hours, ts2.Minutes, ts2.Seconds);
                 }
                 if (isRunning3 == true)
                 {
@@ -55,12 +67,19 @@ namespace AutoServers
                 else
                 {
                     Process.Start(exePath3);
-                    Thread.Sleep(10000);
                     TimeSpan ts3 = TimeSpan.Zero;
-                    lg.AppendJoin("Quantum NPC Started {0}h, {1}m, {2}s.", ts3.Hours, ts3.Minutes, ts3.Seconds);
+                    lg.WriteLine("\n" + DateTime.Now + "\nQuantum NPC Started");
+                    for (x = 0; x < 10; x++)
+                    {
+                        lg.Write(x);
+                    }
+
+                    //close the file
+                    lg.Close();
+                    Thread.Sleep(10000);
                 }
+
                 Thread.Sleep(30000);
-                File.AppendAllText(filePath + "logServerAutostarts.txt", lg.ToString());
             }       
         }
 
