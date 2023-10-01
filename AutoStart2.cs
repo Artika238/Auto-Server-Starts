0959994ed5fec8560050468ddb2738e5b74844c9
Artika <artemch238@gmail.com>
2023-10-01 22:34:10 +0300

    Auto Restart
    - Auto Restart Script

--- AutoStart2.cs
+++ AutoStart2.cs
@@ -19,32 +19,29 @@ namespace Auto_Server_Start
         public static string exePath5 = @"E:\TORCH-Server_13\Torch.Server.exe";
         Int64 x;
 
-        public static void Quantum()
+        public static void QuantumI()
         {
             do
             {
                 bool isRunning = Process.GetProcessesByName("Torch.Server").FirstOrDefault(a => a.MainModule.FileName.StartsWith(@"C:\SERVER9-HARD")) != default(Process);
-                bool isRunning2 = Process.GetProcessesByName("Torch.Server").FirstOrDefault(a => a.MainModule.FileName.StartsWith(@"G:\SERVER11-GALAXY2")) != default(Process);
-                bool isRunning3 = Process.GetProcessesByName("Torch.Server").FirstOrDefault(a => a.MainModule.FileName.StartsWith(@"G:\TORCH-Server_14")) != default(Process);
-                bool isRunning4 = Process.GetProcessesByName("Torch.Server").FirstOrDefault(a => a.MainModule.FileName.StartsWith(@"C:\SERVER10-NPC")) != default(Process);
-                bool isRunning5 = Process.GetProcessesByName("Torch.Server").FirstOrDefault(a => a.MainModule.FileName.StartsWith(@"E:\TORCH-Server_13")) != default(Process);
-                int Hour9 = 12; int Minute9 = 00; int Seconds9 = 00; if ((Hour9 == System.DateTime.Now.Hour) && (Minute9 == System.DateTime.Now.Minute) && (Seconds9 == System.DateTime.Now.Second))
+                int Hour9 = 11; int Minute9 = 59; int Seconds9 = 50; if ((Hour9 == System.DateTime.Now.Hour) && (Minute9 == System.DateTime.Now.Minute) && (Seconds9 == System.DateTime.Now.Second))
                 {
                     Thread.Sleep(60000);
                 }
-                int Hour10 = 18; int Minute10 = 00; int Seconds10 = 00; if ((Hour10 == System.DateTime.Now.Hour) && (Minute10 == System.DateTime.Now.Minute) && (Seconds10 == System.DateTime.Now.Second))
+                int Hour10 = 17; int Minute10 = 59; int Seconds10 = 50; if ((Hour10 == System.DateTime.Now.Hour) && (Minute10 == System.DateTime.Now.Minute) && (Seconds10 == System.DateTime.Now.Second))
                 {
                     Thread.Sleep(60000);
                 }
-                int Hour11 = 00; int Minute11 = 00; int Seconds11 = 00; if ((Hour11 == System.DateTime.Now.Hour) && (Minute11 == System.DateTime.Now.Minute) && (Seconds11 == System.DateTime.Now.Second))
+                int Hour11 = 23; int Minute11 = 59; int Seconds11 = 50; if ((Hour11 == System.DateTime.Now.Hour) && (Minute11 == System.DateTime.Now.Minute) && (Seconds11 == System.DateTime.Now.Second))
                 {
                     ;
                     Thread.Sleep(60000);
                 }
-                int Hour12 = 06; int Minute12 = 00; int Seconds12 = 00; if ((Hour12 == System.DateTime.Now.Hour) && (Minute12 == System.DateTime.Now.Minute) && (Seconds12 == System.DateTime.Now.Second))
+                int Hour12 = 05; int Minute12 = 59; int Seconds12 = 50; if ((Hour12 == System.DateTime.Now.Hour) && (Minute12 == System.DateTime.Now.Minute) && (Seconds12 == System.DateTime.Now.Second))
                 {
                     Thread.Sleep(60000);
                 }
+                else
                 {
                     if (isRunning == true)
                     {
@@ -58,6 +55,34 @@ namespace Auto_Server_Start
                         Thread.Sleep(50000);
                         serv1 = 0;
                     }
+                }
+            }
+            while (true);
+        }
+        public static void QuantumII()
+        {
+            do
+            {
+                bool isRunning2 = Process.GetProcessesByName("Torch.Server").FirstOrDefault(a => a.MainModule.FileName.StartsWith(@"G:\SERVER11-GALAXY2")) != default(Process);
+                int Hour9 = 11; int Minute9 = 59; int Seconds9 = 50; if ((Hour9 == System.DateTime.Now.Hour) && (Minute9 == System.DateTime.Now.Minute) && (Seconds9 == System.DateTime.Now.Second))
+                {
+                    Thread.Sleep(60000);
+                }
+                int Hour10 = 17; int Minute10 = 59; int Seconds10 = 50; if ((Hour10 == System.DateTime.Now.Hour) && (Minute10 == System.DateTime.Now.Minute) && (Seconds10 == System.DateTime.Now.Second))
+                {
+                    Thread.Sleep(60000);
+                }
+                int Hour11 = 23; int Minute11 = 59; int Seconds11 = 50; if ((Hour11 == System.DateTime.Now.Hour) && (Minute11 == System.DateTime.Now.Minute) && (Seconds11 == System.DateTime.Now.Second))
+                {
+                    ;
+                    Thread.Sleep(60000);
+                }
+                int Hour12 = 05; int Minute12 = 59; int Seconds12 = 50; if ((Hour12 == System.DateTime.Now.Hour) && (Minute12 == System.DateTime.Now.Minute) && (Seconds12 == System.DateTime.Now.Second))
+                {
+                    Thread.Sleep(60000);
+                }
+                else
+                {
                     if (isRunning2 == true)
                     {
                         Console.WriteLine("Quantum II Ready");
@@ -70,20 +95,34 @@ namespace Auto_Server_Start
                         serv2 = 0;
                         Thread.Sleep(50000);
                     }
-                    if (isRunning3 == true)
-                    {
-                        Console.WriteLine("Quantum NPC Ready");
-                        serv3 = 1;
-                        Thread.Sleep(1000);
-                    }
-                    else
-                    {
-                        Process.Start(exePath3);
-                        TimeSpan ts3 = TimeSpan.Zero;
-                        serv3 = 0;
-                        Thread.Sleep(50000);
-                    }
-
+                }
+            }
+            while (true);
+        }
+        public static void QuantumIII()
+        {
+            do
+            {
+                bool isRunning4 = Process.GetProcessesByName("Torch.Server").FirstOrDefault(a => a.MainModule.FileName.StartsWith(@"C:\SERVER10-NPC")) != default(Process);
+                int Hour9 = 11; int Minute9 = 59; int Seconds9 = 50; if ((Hour9 == System.DateTime.Now.Hour) && (Minute9 == System.DateTime.Now.Minute) && (Seconds9 == System.DateTime.Now.Second))
+                {
+                    Thread.Sleep(60000);
+                }
+                int Hour10 = 17; int Minute10 = 59; int Seconds10 = 50; if ((Hour10 == System.DateTime.Now.Hour) && (Minute10 == System.DateTime.Now.Minute) && (Seconds10 == System.DateTime.Now.Second))
+                {
+                    Thread.Sleep(60000);
+                }
+                int Hour11 = 23; int Minute11 = 59; int Seconds11 = 50; if ((Hour11 == System.DateTime.Now.Hour) && (Minute11 == System.DateTime.Now.Minute) && (Seconds11 == System.DateTime.Now.Second))
+                {
+                    ;
+                    Thread.Sleep(60000);
+                }
+                int Hour12 = 05; int Minute12 = 59; int Seconds12 = 50; if ((Hour12 == System.DateTime.Now.Hour) && (Minute12 == System.DateTime.Now.Minute) && (Seconds12 == System.DateTime.Now.Second))
+                {
+                    Thread.Sleep(60000);
+                }
+                else
+                {
                     if (isRunning4 == true)
                     {
                         Console.WriteLine("Vanilla Ready");
@@ -97,6 +136,75 @@ namespace Auto_Server_Start
                         server4 = 0;
                         Thread.Sleep(50000);
                     }
+                }
+            }
+            while (true);
+        }
+        public static void QuantumNPC()
+        {
+            do
+            {
+                bool isRunning3 = Process.GetProcessesByName("Torch.Server").FirstOrDefault(a => a.MainModule.FileName.StartsWith(@"G:\TORCH-Server_14")) != default(Process);
+                int Hour9 = 11; int Minute9 = 59; int Seconds9 = 50; if ((Hour9 == System.DateTime.Now.Hour) && (Minute9 == System.DateTime.Now.Minute) && (Seconds9 == System.DateTime.Now.Second))
+                {
+                    Thread.Sleep(60000);
+                }
+                int Hour10 = 17; int Minute10 = 59; int Seconds10 = 50; if ((Hour10 == System.DateTime.Now.Hour) && (Minute10 == System.DateTime.Now.Minute) && (Seconds10 == System.DateTime.Now.Second))
+                {
+                    Thread.Sleep(60000);
+                }
+                int Hour11 = 23; int Minute11 = 59; int Seconds11 = 50; if ((Hour11 == System.DateTime.Now.Hour) && (Minute11 == System.DateTime.Now.Minute) && (Seconds11 == System.DateTime.Now.Second))
+                {
+                    ;
+                    Thread.Sleep(60000);
+                }
+                int Hour12 = 05; int Minute12 = 59; int Seconds12 = 50; if ((Hour12 == System.DateTime.Now.Hour) && (Minute12 == System.DateTime.Now.Minute) && (Seconds12 == System.DateTime.Now.Second))
+                {
+                    Thread.Sleep(60000);
+                }
+                else
+                {
+                    if (isRunning3 == true)
+                    {
+                        Console.WriteLine("Quantum NPC Ready");
+                        serv3 = 1;
+                        Thread.Sleep(1000);
+                    }
+                    else
+                    {
+                        Process.Start(exePath3);
+                        TimeSpan ts3 = TimeSpan.Zero;
+                        serv3 = 0;
+                        Thread.Sleep(50000);
+                    }
+                }
+            }
+            while (true);
+        }
+        public static void QuantumLobby()
+        {
+            do
+            {
+                bool isRunning5 = Process.GetProcessesByName("Torch.Server").FirstOrDefault(a => a.MainModule.FileName.StartsWith(@"E:\TORCH-Server_13")) != default(Process);
+                int Hour9 = 11; int Minute9 = 59; int Seconds9 = 50; if ((Hour9 == System.DateTime.Now.Hour) && (Minute9 == System.DateTime.Now.Minute) && (Seconds9 == System.DateTime.Now.Second))
+                {
+                    Thread.Sleep(60000);
+                }
+                int Hour10 = 17; int Minute10 = 59; int Seconds10 = 50; if ((Hour10 == System.DateTime.Now.Hour) && (Minute10 == System.DateTime.Now.Minute) && (Seconds10 == System.DateTime.Now.Second))
+                {
+                    Thread.Sleep(60000);
+                }
+                int Hour11 = 23; int Minute11 = 59; int Seconds11 = 50; if ((Hour11 == System.DateTime.Now.Hour) && (Minute11 == System.DateTime.Now.Minute) && (Seconds11 == System.DateTime.Now.Second))
+                {
+                    ;
+                    Thread.Sleep(60000);
+                }
+                int Hour12 = 05; int Minute12 = 59; int Seconds12 = 50; if ((Hour12 == System.DateTime.Now.Hour) && (Minute12 == System.DateTime.Now.Minute) && (Seconds12 == System.DateTime.Now.Second))
+                {
+                    Thread.Sleep(60000);
+                }
+                else
+                {
                     if (isRunning5 == true)
                     {
                         Console.WriteLine("Lobby Ready");
--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ using Discord.Rest;
 using Auto_Server_Start;
 using Discord.Utils;
 using System.Threading.Channels;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace AutoServers
 {
@@ -35,7 +36,7 @@ namespace AutoServers
         //public static string exePath1 = @"C:\SERVER9-HARD\Torch.Server.exe";
         //public static string exePath2 = @"G:\SERVER11-GALAXY2\Torch.Server.exe";
         //public static string exePath3 = @"G:\TORCH-Server_14\Torch.Server.exe";
-        //public static string exePath4 = @"C:\SERVER10-NPC\Torch.Server.exe";
+        public static string exePath6 = @"C:\Users\Администратор\Desktop\Server Starts\Auto Server Start.exe";
         //public static DateTime dataTimeQ1 = File.GetLastWriteTime(exePath1);
         //public static DateTime dataTimeQ2 = File.GetLastWriteTime(exePath2);
         //public static DateTime dataTimeVanilla = File.GetLastWriteTime(exePath3);
@@ -48,10 +49,10 @@ namespace AutoServers
         public static int servers2;
         public static int servers3;
         public static int servers4;
-        public static ulong channelId = 1;
-        public static ulong channelId2 = 1;
-        public static ulong channelId3 = 1;
-        public static ulong channelId4 = 1;
+        public static ulong channelId = 857635825301585933;
+        public static ulong channelId2 = 857635825301585933;
+        public static ulong channelId3 = 857635825301585933;
+        public static ulong channelId4 = 857635825301585933;
 
 
 
@@ -68,10 +69,18 @@ namespace AutoServers
         static void Main()
         {
             Thread Main = new Thread(Main2);
-            Thread quant1 = new Thread(Program2.Quantum);
+            Thread quant1 = new Thread(Program2.QuantumI);
+            Thread quant2 = new Thread(Program2.QuantumII);
+            Thread quant3 = new Thread(Program2.QuantumIII);
+            Thread quantNPC = new Thread(Program2.QuantumNPC);
+            Thread quantLOBBY = new Thread(Program2.QuantumLobby);
             Thread TimerS = new Thread(TimerConsole);
             Main.Start();
             quant1.Start();
+            quant2.Start();
+            quant3.Start();
+            quantNPC.Start();
+            quantLOBBY.Start();
             TimerS.Start();
         }
 
@@ -87,66 +96,63 @@ namespace AutoServers
         }
         public static void Main2()
         {
-            //var _client = new DiscordSocketClient();
-            //var token = "";
-            //_client.LoginAsync(TokenType.Bot, token);
-            //_client.StartAsync();
             Console.WriteLine("Бот Включен");
-            //_client.SetGameAsync("Servers worked");
             while (true)
             {
-                GC.Collect();
-                GC.WaitForPendingFinalizers();
-                //Рестарт серверов
                 int Hour9 = 12; int Minute9 = 00; int Seconds9 = 00; if ((Hour9 == System.DateTime.Now.Hour) && (Minute9 == System.DateTime.Now.Minute) && (Seconds9 == System.DateTime.Now.Second))
                 {
+                    Process.Start(exePath6);
+                    Process.GetCurrentProcess().Kill();
+                    Environment.Exit(0);
                     //_client.SetGameAsync("Servers restarted");
-                    Thread.Sleep(60000);
                 }
                 else
                 {
                     var servin = serv1 + serv2 + serv3;
                     //_client.SetGameAsync("Servers " + servin + "/3 Quantum");
-                    Thread.Sleep(1000);
                 }
                 int Hour10 = 18; int Minute10 = 00; int Seconds10 = 00; if ((Hour10 == System.DateTime.Now.Hour) && (Minute10 == System.DateTime.Now.Minute) && (Seconds10 == System.DateTime.Now.Second))
                 {
                     //_client.SetGameAsync("Servers restarted");
-                    Thread.Sleep(60000);
+                    Process.Start(exePath6);
+                    Process.GetCurrentProcess().Kill();
+                    Environment.Exit(0);
                 }
                 else
                 {
                     var servin = serv1 + serv2 + serv3;
                     //_client.SetGameAsync("Servers " + servin + "/3 Quantum");
-                    Thread.Sleep(1000);
                 }
                 int Hour11 = 00; int Minute11 = 00; int Seconds11 = 00; if ((Hour11 == System.DateTime.Now.Hour) && (Minute11 == System.DateTime.Now.Minute) && (Seconds11 == System.DateTime.Now.Second))
-                {;
+                {
                     //_client.SetGameAsync("Servers restarted");
-                    Thread.Sleep(60000);
+                    Process.Start(exePath6);
+                    Process.GetCurrentProcess().Kill();
+                    Environment.Exit(0);
                 }
                 else
                 {
                     var servin = serv1 + serv2 + serv3;
                     //_client.SetGameAsync("Servers " + servin + "/3 Quantum");
-                    Thread.Sleep(1000);
                 }
                 int Hour12 = 06; int Minute12 = 00; int Seconds12 = 00; if ((Hour12 == System.DateTime.Now.Hour) && (Minute12 == System.DateTime.Now.Minute) && (Seconds12 == System.DateTime.Now.Second))
                 {
                     //_client.SetGameAsync("Servers restarted");
-                    Thread.Sleep(60000);
+                    Process.Start(exePath6);
+                    Process.GetCurrentProcess().Kill();
+                    Environment.Exit(0);
                 }
                 else
                 {
                     var servin = serv1 + serv2 + serv3;
                     //_client.SetGameAsync("Servers " + servin + "/3 Quantum");
-                    Thread.Sleep(1000);
                 }
                 var message = (DateTime.Now + "\nQuantum I Started");
                 var message1 = (DateTime.Now + "\nQuantum II Started");
                 var message2 = (DateTime.Now + "\nVanilla Started");
                 var message3 = (DateTime.Now + "\nQuantum NPC Started");
 
+                Thread.Sleep(1);
                 {
                     // Block this task until the program is closed.
 
@@ -155,9 +161,9 @@ namespace AutoServers
                     //                StreamWriter lg = new StreamWriter(filePath + "logServerAutostarts.txt", true, Encoding.ASCII);
                 }
             }
+            }
 
         }
-    }
 
 }
 
